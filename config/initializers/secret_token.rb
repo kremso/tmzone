5002ee7d6528e9d2df5a665d0d8f6a2c918cba63
Tomas Kramar <kramar.tomas@gmail.com>
2012-05-24 16:34:07 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tmzone::Application.config.secret_token = '900c3b67d47a5af4a25bc8749b0088b4672187d819c3405ba5295adb7ee860a93f7c29329a395582ccdf173929d54c8fed8b781dfecd2665dc1867b0799bdf5c'
